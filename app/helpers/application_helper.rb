0ba76b0a5e92bffa916ff97135c595742e9ff151
Kinsey Durham <kinseyann505@gmail.com>
2013-07-02 14:23:26 -0600

    refactoring of the nav bar so routes work and is cleaner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_if_current_link(id, text, options = {})
+    options[:class] ||= []
+    if controller_name == id
+      options[:class].push(:active)
+    end
+    link_to text, "##{id}", options
+  end
 end
