e0c1444c919aa93adaf20f74789ad37a5335249a
Kinsey Durham <kinseyann505@gmail.com>
2013-07-04 10:22:56 -0600

    Fixed static_pages spec based on copy changes

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -8,7 +8,7 @@ describe 'Static pages' do
 
       find_link('how it works').visible?
       find_link('entrepreneurs').visible?
-      find_link('about us').visible?
+      find_link('about').visible?
       find_link('apply').visible?
       find_link('media').visible?
       find_button('donate').click
