e8c20e6820cdab1576481b4b13ee0b3d5ab11943
Kinsey Durham <kinseyann505@gmail.com>
2013-08-11 11:25:45 -0600

    Allow real keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,2 @@
-Stripe.api_key = "sk_test_TtGqGxYISKt8yyVxMptyzcwb"
-STRIPE_PUBLIC_KEY = "pk_test_TZnvFJB8ugM4vAoe9kFwkmnX"
+Stripe.api_key = ENV['STRIPE_API_KEY'] || "sk_test_TtGqGxYISKt8yyVxMptyzcwb"
+STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY'] || "pk_test_TZnvFJB8ugM4vAoe9kFwkmnX"
